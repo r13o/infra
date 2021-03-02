82e130ff6260cf8d7944ca12a08212cf206dd52f
Romuald Bulyshko <romuald@bulyshko.com>
2021-03-02 21:49:50 +0200

    Manage domain by Terraform

--- main.tf
+++ main.tf
@@ -2,22 +2,19 @@ provider "cloudflare" {
   api_token = var.cf_token
 }
 
-data "cloudflare_zones" "r13o" {
-  filter {
-    name   = "r13o.com"
-    status = "active"
-  }
+resource "cloudflare_zone" "domain" {
+  zone = "r13o.com"
 }
 
-resource "cloudflare_record" "r13o" {
-  zone_id = data.cloudflare_zones.r13o.zones.0.id
+resource "cloudflare_record" "root" {
+  zone_id = cloudflare_zone.domain.id
   name    = "@"
   type    = "CNAME"
   value   = "r13o.github.io"
 }
 
-resource "cloudflare_record" "www_r13o" {
-  zone_id = data.cloudflare_zones.r13o.zones.0.id
+resource "cloudflare_record" "www" {
+  zone_id = cloudflare_zone.domain.id
   name    = "www"
   type    = "CNAME"
   value   = "r13o.com"

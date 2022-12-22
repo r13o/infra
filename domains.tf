33b3072c68fdc545d10d7bb4add72b396d907c53
Romuald Bulyshko <git42df6@romuald.xyz>
2022-12-22 20:03:44 +0200

    Override settings for security and speed

--- domains.tf
+++ domains.tf
@@ -2,6 +2,19 @@ resource "cloudflare_zone" "short" {
   zone = "r13o.com"
 }
 
+resource "cloudflare_zone_settings_override" "short" {
+  zone_id = cloudflare_zone.short.id
+  settings {
+    always_use_https         = "on"
+    automatic_https_rewrites = "on"
+    minify {
+      css  = "on"
+      js   = "on"
+      html = "on"
+    }
+  }
+}
+
 module "alias" {
   source       = "./modules/cloudflare-alias"
   domain_name  = "romualdbulyshko.com"

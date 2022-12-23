7870374b949139ddb99fc708c649b5405bfa5113
Romuald Bulyshko <git42df6@romuald.xyz>
2022-12-23 17:36:15 +0200

    Force non-www domain

--- website.tf
+++ website.tf
@@ -39,6 +39,17 @@ resource "cloudflare_record" "www" {
   proxied = true
 }
 
+resource "cloudflare_page_rule" "redirect_www" {
+  zone_id = cloudflare_zone.short.id
+  target  = "*${cloudflare_record.www.hostname}/*"
+  actions {
+    forwarding_url {
+      url         = "https://${cloudflare_record.root.hostname}/$2"
+      status_code = 301
+    }
+  }
+}
+
 resource "cloudflare_pages_domain" "short" {
   account_id   = local.cf_account_id
   project_name = cloudflare_pages_project.website.name

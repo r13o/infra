32470d3ccce764931c60ff505defd533e1e51451
Romuald Bulyshko <git42df6@romuald.xyz>
2022-12-23 22:08:39 +0200

    Unlink www from Cloudflare Pages

--- website.tf
+++ website.tf
@@ -37,17 +37,17 @@ resource "cloudflare_record" "root" {
 resource "cloudflare_record" "www" {
   zone_id = cloudflare_zone.short.id
   name    = "www"
-  type    = "CNAME"
-  value   = cloudflare_record.root.hostname
+  type    = "A"
+  value   = "192.0.2.1"
   proxied = true
 }
 
 resource "cloudflare_page_rule" "redirect_www" {
   zone_id = cloudflare_zone.short.id
-  target  = "*${cloudflare_record.www.hostname}/*"
+  target  = "${cloudflare_record.www.hostname}/*"
   actions {
     forwarding_url {
-      url         = "https://${cloudflare_record.root.hostname}/$2"
+      url         = "https://${cloudflare_record.root.hostname}/$1"
       status_code = 301
     }
   }
@@ -57,10 +57,4 @@ resource "cloudflare_pages_domain" "short" {
   account_id   = local.cf_account_id
   project_name = cloudflare_pages_project.website.name
   domain       = cloudflare_record.root.hostname
-}
-
-resource "cloudflare_pages_domain" "short_www" {
-  account_id   = local.cf_account_id
-  project_name = cloudflare_pages_project.website.name
-  domain       = cloudflare_record.www.hostname
 }

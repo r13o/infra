f434855911100514e35febd4419453d32c98b11b
Romuald Bulyshko <git42df6@romuald.xyz>
2023-01-07 17:48:06 +0200

    Add missing dependency

--- modules/cloudflare-tunnel/versions.tf
+++ modules/cloudflare-tunnel/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = ">= 3.31.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.2.1"
+    }
   }
   required_version = ">= 1.1.0"
 }

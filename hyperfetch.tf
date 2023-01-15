c92fd1ea47700e388395158ca92827f257ce33cc
Romuald Bulyshko <git42df6@romuald.xyz>
2023-01-15 17:10:04 +0200

    Deploy hyperfetch.net website

--- hyperfetch.tf
+++ hyperfetch.tf
@@ -16,3 +16,31 @@ module "hyperfetch_repository" {
   source = "./modules/github-repository"
   name   = "hyperfetch"
 }
+
+resource "cloudflare_pages_project" "hyperfetch" {
+  account_id        = local.cf_account_id
+  name              = "hyperfetch"
+  production_branch = "main"
+  source {
+    type = "github"
+    config {
+      owner             = module.hyperfetch_repository.owner
+      repo_name         = module.hyperfetch_repository.name
+      production_branch = "main"
+    }
+  }
+}
+
+resource "cloudflare_record" "hyperfetch" {
+  zone_id = cloudflare_zone.hyperfetch.id
+  name    = "@"
+  type    = "CNAME"
+  value   = cloudflare_pages_project.hyperfetch.subdomain
+  proxied = true
+}
+
+resource "cloudflare_pages_domain" "hyperfetch" {
+  account_id   = local.cf_account_id
+  project_name = cloudflare_pages_project.hyperfetch.name
+  domain       = cloudflare_record.hyperfetch.hostname
+}

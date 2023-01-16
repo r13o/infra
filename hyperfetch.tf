9f5724772e6a1a706b86af5cd408359a2a03e726
Romuald Bulyshko <git42df6@romuald.xyz>
2023-01-16 21:14:18 +0200

    Remove zone settings override

--- hyperfetch.tf
+++ hyperfetch.tf
@@ -4,14 +4,6 @@ resource "cloudflare_zone" "hyperfetch" {
   plan       = "free"
 }
 
-resource "cloudflare_zone_settings_override" "hyperfetch" {
-  zone_id = cloudflare_zone.hyperfetch.id
-  settings {
-    always_use_https         = "off"
-    automatic_https_rewrites = "off"
-  }
-}
-
 module "hyperfetch_repository" {
   source      = "./modules/github-repository"
   name        = "hyperfetch"
